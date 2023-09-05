6dfcf1ab0509a1f9c2150682b28300e185e3edbb
marsade <adenijifara@gmail.com>
2023-09-05 07:29:18 -0700

    fixed edge case ⚡

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,19 +15,18 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *node;
 	listint_t *temp;
 
-	ptr = *head;
-	temp = ptr->next;
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
 	node->n = number;
 	node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = node;
 		return (node);
 	}
+	ptr = *head;
+	temp = ptr->next;
 	while (ptr->next != NULL)
 	{
 		if (node->n <= ptr->n)
