be73540c2b5bd3efcc52a3a8ccdd75e3e75e06e0
marsade <adenijifara@gmail.com>
2023-09-06 06:00:27 -0700

    task 8 complete

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ''
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            upp = chr(ord(i) - 32)
+            new_str += upp
+        else:
+            new_str += i
+    print(new_str)
