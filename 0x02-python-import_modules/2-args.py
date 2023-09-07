f60a1714c383ebcbcd2a97bd85f3064d2e982eb4
marsade <adenijifara@gmail.com>
2023-09-07 07:13:11 -0700

    optimise code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    n = len(argv) - 1
+    n = len(argv)
     if n == 1:
         print("0 arguments.")
     else:
         print("{} arguments:".format(n))
-        for i in range(1, n + 1):
+        for i in range(1, n):
             print("{}: {}".format(i, argv[i]))
