be8183ee68b3575a6735ceff435f04573801a6b2
marsade <adenijifara@gmail.com>
2023-09-07 01:13:17 -0700

    -m

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ import add_0 as add_pro
 if __name__ == "__main__":
     a = 1
     b = 2
-    c = add_pro.add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, add_pro.add(a, b)))
