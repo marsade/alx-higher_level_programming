caa2a519a4be96d706d8f07fbbc63646c19ab310
marsade <adenijifara@gmail.com>
2023-09-14 08:52:24 -0700

    task 7 complete

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key == a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
