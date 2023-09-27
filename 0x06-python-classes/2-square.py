56a79232c41d8c8537f6fb7374d860827906f574
marsade <adenijifara@gmail.com>
2023-09-27 20:55:38 +0300

    pycode validated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,8 @@ class Square:
     __size = None
 
     def __init__(self, size=0):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
