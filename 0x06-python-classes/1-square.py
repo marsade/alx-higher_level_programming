0a7a7e7c3041effdc9f716688de3dd84544fc3ff
marsade <adenijifara@gmail.com>
2023-09-27 20:21:21 +0300

    pycode validated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,8 +7,18 @@
 
 
 class Square:
-    """square class"""
+    """square class.
+
+    Attributes:
+        size(int): size of square
+    """
     __size = None
 
     def __init__(self, size=None):
+        """Initialize square
+
+        Args:
+            size(int): size of square
+
+        """
         self.__size = size
