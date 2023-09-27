400ad6edd84ead5592160b6ae653aa4039c9b25a
marsade <adenijifara@gmail.com>
2023-09-27 20:53:19 +0300

    modify task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,13 +11,8 @@ class Square:
     __size = None
 
     def __init__(self, size=0):
-        try:
             if not isinstance(size, int):
                 raise TypeError("size must be an integer")
             elif size < 0:
                 raise ValueError("size must be >= 0")
             self.__size = size
-        except ValueError as ve:
-            print(ve)
-        except TypeError as te:
-            print(te)
