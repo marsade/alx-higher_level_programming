a9b68a3c23c98cbc2b4833a75e42254ab8081945
marsade <adenijifara@gmail.com>
2023-09-27 20:51:41 +0300

    task 2 complete

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,8 +16,7 @@ class Square:
                 raise TypeError("size must be an integer")
             elif size < 0:
                 raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+            self.__size = size
         except ValueError as ve:
             print(ve)
         except TypeError as te:
