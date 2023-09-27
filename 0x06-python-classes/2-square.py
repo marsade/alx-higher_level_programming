ef428fd6a15983b0f6568cc6d1a54bce7af269fa
marsade <adenijifara@gmail.com>
2023-09-27 20:49:46 +0300

    task 2 complete

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """square class
+
+    Attributes:
+        size(int): size of the square
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except ValueError as ve:
+            print(ve)
+        except TypeError as te:
+            print(te)
