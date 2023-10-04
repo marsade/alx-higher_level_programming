30831284a48198e53e8e5cc815d4f19c4ecff341
marsade <adenijifara@gmail.com>
2023-10-04 20:07:37 +0300

    task 5 complete

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""area and perimeter of a rectangle"""
+
+
+class Rectangle:
+    """rectangleclass"""
+    def __init__(self, width=0, height=0):
+        """initializes a rectangle
+
+            Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area of the rectangle
+
+        Returns:
+            area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            if i != self.__height - 1:
+                print("")
+        return ""
+
+    def __repr__(self):
+        res = "Rectangle("
+        res += str(self.__width) + ", " + str(self.__height) + ")"
+        return res
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+del my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
