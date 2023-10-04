1a09eb13b11f3f3ef7e9fddb3711c53e3efc4e61
marsade <adenijifara@gmail.com>
2023-10-04 20:08:43 +0300

    task 5 complete

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -70,13 +70,3 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-del my_rectangle
-
-try:
-    print(my_rectangle)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
