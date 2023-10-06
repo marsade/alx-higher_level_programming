a48344c1d54abf471141eb2f5186a0cbe84a50fd
marsade <adenijifara@gmail.com>
2023-10-07 01:26:42 +0300

    modify tests

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,5 +10,16 @@ def lazy_matrix_mul(m_a, m_b):
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     """
-
+    if m_b == [] or m_b == [[]]:
+        TypeError("m_b cannot be empty")
+    if (not isinstance(m_a, list) or m_a == [] or
+            not all(isinstance(row, list) for row in m_a) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in m_a for num in row])):
+        raise TypeError("m_a must be a list")
+    if (not isinstance(m_b, list) or m_b == [] or
+            not all(isinstance(row, list) for row in m_b) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in m_b for num in row])):
+        raise TypeError("m_b must be a list")
     return (np.matmul(m_a, m_b))
