6d7c208609d671e16a57bd33ecfb997f04631834
marsade <adenijifara@gmail.com>
2023-10-07 00:08:43 +0300

    pycode validated

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,15 @@
 
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name,
-                      str) and isinstance(last_name, str):
-        raise TypeError ("first_name must be a string")
-    if isinstance(first_name, 
-                    str) and not isinstance(last_name, str):
-        raise TypeError ("last_name must be a string")
+    """prints My name is first_name and last_name
+
+    Args:
+        first_name(str): First name
+        last_name(str): Last name
+    Returns:
+        Nothing"""
+    if not isinstance(first_name, str) and isinstance(last_name, str):
+        raise TypeError("first_name must be a string")
+    if isinstance(first_name, str) and not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
