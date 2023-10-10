e8840368a8e07a2d6f45af16b3c8470aba6a2fc4
marsade <adenijifara@gmail.com>
2023-10-11 00:53:18 +0300

    task 0 complete

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""reads a file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints it to stdout"""
+
+    with open(filename) as f:
+        for line in f:
+            print(line)
