65eb3720002455885279d3af1da08207b5024480
marsade <adenijifara@gmail.com>
2023-10-11 01:40:36 +0300

    modify code

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,4 +9,4 @@ def to_json_string(my_obj):
     Args:
         my_obj: object to convert
     """
-    json.dumps(my_obj)
+    return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,4 +9,4 @@ def from_json_string(my_str):
     Args:
         my_obj: object to convert
     """
-    json.loads(my_str)
+    return json.loads(my_str)
