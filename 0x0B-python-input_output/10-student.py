79af6c30c392a5873510ae26e194c90baf0c5cc4
marsade <adenijifara@gmail.com>
2023-10-11 02:39:56 +0300

    task 10 complete

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initialize Student
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary rep of student instance"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
