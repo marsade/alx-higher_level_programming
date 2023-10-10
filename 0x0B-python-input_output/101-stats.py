cef49fc322360d7f6fad86944b6b95a33fcd9571
marsade <adenijifara@gmail.com>
2023-10-11 02:52:59 +0300

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
+"""Reads from standard input and computes metrics."""
 
 
 def print_stats(size, status_codes):
