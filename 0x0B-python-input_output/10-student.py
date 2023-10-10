ba50abcbfefb4fc41eb9b559fe22e62eff01d3c2
marsade <adenijifara@gmail.com>
2023-10-11 02:41:24 +0300

    pycode valid

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,8 +16,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves dictionary rep of student instance"""
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
