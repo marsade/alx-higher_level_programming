b59d15e7f6df54385d1be4d76d8962478691d819
marsade <adenijifara@gmail.com>
2023-10-11 01:35:11 +0300

    pycode validated

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """json representation of an object"""
-
-
 import json
+
+
 def to_json_string(my_obj):
     """converts to json string representation
 
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""decode json representation of an object"""
+import json
+
+
+def from_json_string(my_str):
+    """converts from json string representation
+
+    Args:
+        my_obj: object to convert
+    """
+    json.loads(my_str)
