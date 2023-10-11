8a433300145c8be2b504f47416c3ec289c40607a
marsade <adenijifara@gmail.com>
2023-10-12 00:34:21 +0300

    task 1 complete

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""My list"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """Prints a sorted list"""
+        sorted_list = sorted(self)
+        print(sorted_list)
