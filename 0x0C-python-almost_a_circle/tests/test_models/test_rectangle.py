968c508855f20b9412fb6afa04c4189d7221dd99
marsade <adenijifara@gmail.com>
2023-10-17 08:50:19 +0300

    modify tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittests for Base class"""
+"""Unittests for Rectangle class"""
 import unittest
 from models.rectangle import Rectangle
 
@@ -8,13 +8,12 @@ class RectangleTest(unittest.TestCase):
     def test_init(self):
         r1 = Rectangle(10, 2)
         r2 = Rectangle(2, 10)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r1.id, r2.id - 1)
 
     def test_id(self):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
-        
+    
 
 if __name__ == '__main__':
     unittest.main()
