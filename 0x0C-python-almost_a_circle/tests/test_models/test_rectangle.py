7d545660cdea6cc186b5303ccdfe5201d23ff76c
marsade <adenijifara@gmail.com>
2023-10-17 09:20:49 +0300

    update test_rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,7 @@
 """Unittests for Rectangle class"""
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class RectangleTest(unittest.TestCase):
@@ -10,10 +11,23 @@ class RectangleTest(unittest.TestCase):
         r2 = Rectangle(2, 10)
         self.assertEqual(r1.id, r2.id - 1)
 
+    def test_three_args(self):
+        r1 = Rectangle(1, 2, 3)
+        r2 = Rectangle(4, 5, 6)
+        self.assertEqual(r1.id, r2.id - 1)
+
+    def test_five_args(self):
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        r2 = Rectangle(5, 7, 8, 9, 10)
+        self.assertEqual(r1.id, 5)
+        self.assertEqual(r2.id, 10)
+
     def test_id(self):
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
-    
+
+    def test_isstance(self):
+        self.assertIsInstance(Rectangle(1, 9), Base)
 
 if __name__ == '__main__':
     unittest.main()
