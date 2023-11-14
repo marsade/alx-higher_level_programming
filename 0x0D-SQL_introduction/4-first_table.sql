f68afc203efc299255a924c7c088da76e1d4d0bc
marsade <adenijifara@gmail.com>
2023-11-14 12:39:41 -0800

    Task complete

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 --Create a table in the current database
-CREATE TABLE IF NOT EXISTS first_table(
-	id INT,
-	name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table`(
+	`id` INT,
+	`name` VARCHAR(256)
 	);
