1629b0a274c11b7ae23f413c3cde42387a35faa7
marsade <adenijifara@gmail.com>
2023-11-15 10:08:26 -0800

    Input correct query

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Lists all the records of the table second_table in order of score
-SELECT `score`, `name` FROM `second_table` ORDER `score`;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
