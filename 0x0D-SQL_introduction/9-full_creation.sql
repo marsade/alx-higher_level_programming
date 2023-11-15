2cbe6e29c667c9b8ab7059059fdff9ecc0d492f2
marsade <adenijifara@gmail.com>
2023-11-15 09:49:43 -0800

    Task 9 complete

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Creates a second table in hbtn_0c_0
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
+);
+INSERT INTO `second_table` VALUES (1, "John", 10), (2, "Alex", 3),
+(3, "Bob", 14), (4, "George", 8);
