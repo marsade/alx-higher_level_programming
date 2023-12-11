3491f5c06597dbe2c5a2c1061aafa1907d55bee7
marsade <adenijifara@gmail.com>
2023-12-11 23:39:35 +0200

    Task complete

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const factorial = (n) => {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+const arg = parseInt(process.argv[2], 10);
+console.log(factorial(arg));
