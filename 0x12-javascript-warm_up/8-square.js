7e5da6114028bfddcd5ad9c8c123b8ed1dac98aa
marsade <adenijifara@gmail.com>
2023-12-11 23:34:28 +0200

    Task complete

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = Number(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
