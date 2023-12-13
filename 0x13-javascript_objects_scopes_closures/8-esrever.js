0120527bacb6cae4f35be179037060d2238331df
marsade <adenijifara@gmail.com>
2023-12-13 11:55:17 +0200

    Semistandard fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const len = list.length - 1;
   const newList = [];
-  for (let i = len; i != -1; i--) {
+  for (let i = len; i !== -1; i--) {
     newList.push(list[i]);
   }
   return newList;
