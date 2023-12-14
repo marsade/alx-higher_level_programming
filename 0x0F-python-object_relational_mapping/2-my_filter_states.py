2d13f4b570a5570080e1d8d391be6624fa05a475
marsade <adenijifara@gmail.com>
2023-12-14 22:50:45 +0200

    Task 2 complete

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""Lists all states where name matches the argument from the database hbtn_0e_0_usa"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        exit
+    usr = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', port=3306, user=usr, passwd=password, db=db_name)
+    cur = db.cursor()
+    exe = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(exe)
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
+    cur.close()
+    db.close()
