f2c601b752635d84084887e328a9b548e4d33d0a
marsade <adenijifara@gmail.com>
2023-12-18 00:56:26 +0200

    Task 11 complete

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""lists all states objects that contain the letter a """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-       sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    sess = Session()
-
-    new_state = State(name="Lousiana")
-    sess.add(new_state)
-    new_instance = sess.query(State).filter_by(name='Louisiana').first()
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    new_instance = session.query(State).filter_by(name='Louisiana').first()
     print(new_instance.id)
-    sess.commit()
+    session.commit()
