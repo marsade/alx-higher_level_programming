61e0d7a321b5168640b025d380f303173165f937
marsade <adenijifara@gmail.com>
2023-12-17 13:01:45 +0200

    Task 5 revamped

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""lists all cities from a state in hbtn_0e_4_usa"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        exit
-    usr = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    match = sys.argv[4]
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=usr, passwd=password, db=db_name)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT cities.name \
-                FROM cities \
-                INNER JOIN states \
-                ON states.id = cities.state_id \
-                WHERE states.name = %s ", (match, ))
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
-    tmp = set(row[0] for row in rows)
+    tmp = list(row[0] for row in rows)
     print(*tmp, sep=", ")
     cur.close()
     db.close()
