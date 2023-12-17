16f477fcf70d8aa332e6f5f4a00e2e461c285edc
marsade <adenijifara@gmail.com>
2023-12-17 11:39:28 +0200

    Minor changes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states where name matches the argument from the database hbtn_0e_0_usa"""
+"""states where name matches the argument from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -11,11 +11,12 @@ if __name__ == '__main__':
     password = sys.argv[2]
     db_name = sys.argv[3]
     match = sys.argv[4]
-    db = MySQLdb.connect(host='localhost', port=3306, user=usr, passwd=password, db=db_name)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=usr, passwd=password, db=db_name)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
     cur.close()
     db.close()
