f0f7e409c79b8f3e412bbb99559c2cfdaa20d992
marsade <adenijifara@gmail.com>
2023-12-17 12:56:06 +0200

    Task 5 complete

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all cities from a state in hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        exit
+    usr = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    match = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=usr, passwd=password, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT cities.name \
+                FROM cities \
+                INNER JOIN states \
+                ON states.id = cities.state_id \
+                WHERE states.name = %s ", (match, ));
+    rows = cur.fetchall()
+    tmp = set(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
