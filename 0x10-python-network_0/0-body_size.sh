36db1a36a3d8fb880b025613637a194954955251
marsade <adenijifara@gmail.com>
2024-01-05 19:18:34 +0200

    Pipe into grep

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a url and displays the size of the body of the response
-curl -w "%{size_download}\n" -sI "$1"
+curl -w "%{size_download}\n" "$1" -s | grep -oE "[0-9]+"
