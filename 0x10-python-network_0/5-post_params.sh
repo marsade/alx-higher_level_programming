b05774871010a2b00c1051d992104f7cafa556b6
marsade <adenijifara@gmail.com>
2024-01-05 21:13:14 +0200

    Add silent mode option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
