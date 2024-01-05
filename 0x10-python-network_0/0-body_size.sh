0145dca16ed562675c0b93af0bb0aa79d3d97785
marsade <adenijifara@gmail.com>
2024-01-05 19:03:10 +0200

    Add shebang and silent mode for curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script takes in a url and displays the size of the body of the response
-
-curl -w "%{size_download}" "$2"
-echo ""
+curl -w "%{size_download}\n" "$2" -s
