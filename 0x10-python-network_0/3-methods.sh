aab920da1609c0f8b74dcd69c9afc6d124722256
marsade <adenijifara@gmail.com>
2024-01-05 19:55:47 +0200

    Fix typo

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays the body of a response
-curl --I -s -X OPTIONS "$1" | grep -i "^Allow:" | sed "s/^Allow: //"
+curl -I -s -X OPTIONS "$1" | grep -i "^Allow:" | sed "s/^Allow: //"
