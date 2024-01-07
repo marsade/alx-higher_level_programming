4cad5c33f617de11b8549c189748d8fb05bc31b4
marsade <adenijifara@gmail.com>
2024-01-07 15:41:52 +0200

    Task 6 complete

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module finds the peak of a list"""
+
+
+def find_peak(list_of_integers):
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = low + (high - low) // 2
+        mid = int(mid)
+        if ((mid == 0 or list_of_integers[mid - 1] <= list_of_integers[
+            mid]) and (mid == len(list_of_integers) - 1 or list_of_integers[
+                mid + 1] <= list_of_integers[mid])):
+            return list_of_integers[mid]
+        elif (mid > 0 and list_of_integers[mid + 1] > list_of_integers[mid]):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    if low >= high:
+        return None
