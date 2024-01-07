66d74e336e0924e811cc7a1523aa8f95e8cb1e47
marsade <adenijifara@gmail.com>
2024-01-08 00:49:32 +0200

    Task 2 complete

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
+        print(page.decode('utf-8'))
