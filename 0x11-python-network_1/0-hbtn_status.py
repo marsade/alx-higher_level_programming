354c199323c8716f5ce3f0f3580054c94ec70900
marsade <adenijifara@gmail.com>
2024-01-07 23:47:46 +0200

    Update print statements with escape character

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This script fetches from the https://alx-intranet.hbtn.io/status site"""
-import urllib.request
 
 if __name__ == '__main__':
+    import urllib.request
     url = "https://alx-intranet.hbtn.io/status"
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as repsonse:
         page = repsonse.read()
         print("Body response:")
-        print("    - type: {}".format(type(page)))
-        print("    - content: {}".format(page))
-        print("    - utf8 content: {}".format(page.decode('utf8')))
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode('utf8')))
