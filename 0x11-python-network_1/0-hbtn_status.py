40856d6f6545d301a34bc87bf58e700474d498e8
marsade <adenijifara@gmail.com>
2024-01-07 23:39:35 +0200

    Task 0 complete

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script fetches from the https://alx-intranet.hbtn.io/status site"""
+import urllib.request
+
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as repsonse:
+        page = repsonse.read()
+        print("Body response")
+        print("    - type: {}".format(type(page)))
+        print("    - content {}".format(page))
+        print("    - utf8 content: {}".format(page.decode('utf8')))
