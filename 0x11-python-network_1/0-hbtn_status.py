701d13c6b1fd314c4360d842892aa90de5136bb2
marsade <adenijifara@gmail.com>
2024-01-07 23:41:32 +0200

    edit print statemeny

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as repsonse:
         page = repsonse.read()
-        print("Body response")
+        print("Body response:")
         print("    - type: {}".format(type(page)))
         print("    - content {}".format(page))
         print("    - utf8 content: {}".format(page.decode('utf8')))
