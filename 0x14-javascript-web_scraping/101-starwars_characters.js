2f56b31add830eea767bfb27c7f8e503a8e7dad5
marsade <adenijifara@gmail.com>
2024-01-31 00:26:15 +0200

    Minor changes

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
 const request = require('request');
-if (args.length > 1) {
-  console.log('Usage: ./0-readme.js URL');
-} else {
-  const url = 'https://swapi-api.alx-tools.com/api/films/';
-  request(url + args[0], function (error, res, body) {
-    if (error) {
-      console.log(error);
-    } else {
-      const charArr = JSON.parse(body).characters;
-      charArr.forEach(element => {
-        request(element, function (err, res, body) {
-          if (err) {
-            console.log(err);
-          } else {
-            const char = JSON.parse(body).name;
-            console.log(char);
-          }
-        });
-      });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
   });
 }
